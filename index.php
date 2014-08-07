03a431a8f5a616b4115b87e6902458e47c2202d1
Vinicius Sorrentino <inshideru@gmail.com>
2014-08-07 09:42:27 -0300

    Modificado link para bootstrap

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Git Lab</title>
-		<link rel="stylesheet" href="../PhpStudy/bootstrap-3.1.1/css/bootstrap.css" >
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">	
 	</head>
 	<body>
 
