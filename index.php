3629cf8ef1e866250fa268f05da0087a8f6cd02d
Vinicius Sorrentino <inshideru@gmail.com>
2014-08-07 00:01:24 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Git Lab</title>
+		<link rel="stylesheet" href="../PhpStudy/bootstrap-3.1.1/css/bootstrap.css" >
+	</head>
+	<body>
+
+
+
+<div class="container">
+	<div class="row">
+			<div class="jumbotron">
+
+			Git Lab
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12">
+			Isto é um teste!
+		</div>
+	</div>
+	</body>
+
+</html>
